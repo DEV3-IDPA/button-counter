d3da61b5ecd4f7b039fd9d44a3802801375238e0
StijnVP <stijnvanpeborgh@gmail.com>
2024-05-08 14:02:35 +0200

    buttons and callbacks

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+  function increaseCounter() {
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+    setCounter(currentCount => {
+      return currentCount + 1;
+    });
+    setCounter(currentCount => {
+      return currentCount + 1;
+    });
+  }
+  function decreaseCounter() {
+    // setCounter(counter - 1);
+    // setCounter(counter - 1);
+    setCounter(currentCount => {
+      return currentCount - 1;
+    });
+    setCounter(currentCount => {
+      return currentCount - 1;
+    });
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Button title="click me!" onPress={() => setCounter(counter + 1)} /> */}
+      <Button style="button" title="increase me!" onPress={() => increaseCounter()} />
+      <Button style="button" title="decrease me!" onPress={() => decreaseCounter()} />
+      {/* <Button style="button" title="click me!" onPress={() => setCounter((currentCounter) => currentCounter + 1)} /> */}
+      <Text style="text">you clicked {counter} times</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +44,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
